05c5c0af37c12fd59a5c826be7352c1297755515
Muking3 <marcmusasa04@gmail.com>
2023-11-19 20:20:21 +0200

    upgrade

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const array_contact = []
 const object_contact = {
     first_name: document.querySelector("#first_nam"),
     names: document.querySelector("#names"),
+    numbers: document.querySelector("#numbers"),
+    email: document.querySelector("#email"),
 }
 console.log(names);
 const p_fn_3 = document.querySelector(".p_fn_3")
@@ -52,6 +54,94 @@ names.addEventListener("blur", function () {
         p_n_50.hidden = true
     }
 })
+
+const p_num = document.querySelector(".p_num")
+p_num.hidden = true
+const p_num_v = document.querySelector(".p_num_v")
+p_num_v.hidden = true
+const p_num_f = document.querySelector(".p_num_f")
+p_num_f.hidden = true
+const p_num_u = document.querySelector(".p_num_u")
+p_num_u.hidden = true
+numbers.addEventListener("blur", function () {
+    if (isNaN(numbers.value)) {
+        numbers.style.border = "2px solid red"
+        numbers.style.borderRadius = "5px"
+        p_num_v.hidden = false
+        p_num_f.hidden = true
+        p_num_u.hidden = true
+        p_num.hidden = true
+    }
+    else if (numbers.value.length != 10) {
+        numbers.style.border = "2px solid red"
+        numbers.style.borderRadius = "5px"
+        p_num.hidden = false
+        p_num_v.hidden = true
+        p_num_f.hidden = true
+        p_num_u.hidden = true
+    }
+    else if (numbers.value.substring(0, 3) != "084" && numbers.value.substring(0, 3) != "085" && numbers.value.substring(0, 3) != "080" && numbers.value.substring(0, 3) != "089" && numbers.value.substring(0, 3) != "081" && numbers.value.substring(0, 3) != "082" && numbers.value.substring(0, 3) != "099" && numbers.value.substring(0, 3) != "097" && numbers.value.substring(0, 3) != "090") {
+        // console.log(typeof(numbers.value));
+        numbers.style.border = "2px solid red"
+        numbers.style.borderRadius = "5px"
+        p_num_f.hidden = false
+        p_num_v.hidden = true
+        p_num.hidden = true
+        p_num_u.hidden = true
+    }
+
+    // else if (numbers.value.substring(0,3) != "084" && numbers.value.substring(0,3) != "085" && numbers.value.substring(0,3) != "080" && numbers.value.substring(0,3) != "089" && numbers.value.substring(0,3) != "081" && numbers.value.substring(0,3) != "082" && numbers.value.substring(0,3) != "099" && numbers.value.substring(0,3) != "097" && numbers.value.substring(0,3) != "090") {
+    //     // console.log(typeof(numbers.value));
+    //     numbers.style.border = "2px solid red"
+    //     numbers.style.borderRadius = "5px"
+    //     p_num_f.hidden = false
+    //     p_num_v.hidden = true
+    //     p_num.hidden = true
+    //     p_num_u.hidden = true
+    // }
+
+    else {
+        numbers.style.border = ""
+        numbers.style.borderRadius = ""
+        p_num.hidden = true
+        p_num_v.hidden = true
+        p_num_f.hidden = true
+        p_num_u.hidden = true
+    }
+})
+
+const p_email = document.querySelector(".p_email")
+p_email.hidden = true
+const p_email_u = document.querySelector(".p_email_u")
+p_email_u.hidden = true
+email.addEventListener("blur", function () {
+    // if () {
+    //     email.style.border = "2px solid red"
+    //     email.style.borderRadius = "5px"
+    //     p_email_u.hidden = true
+    //     p_email.hidden = false
+    // }
+
+    // // else if (isNaN(email.value)) {
+    // //     email.style.border = "2px solid red"
+    // //     email.style.borderRadius = "5px"
+    // //     p_email_u.hidden = true
+    // //     p_email.hidden = true
+    // // }
+
+    // else {
+    //     email.style.border = ""
+    //     email.style.borderRadius = ""
+    //     p_email_u.hidden = true
+    //     p_email.hidden = true
+    // }
+})
+
+
+
+
+
+
 
 
 
